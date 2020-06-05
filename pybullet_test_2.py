20b50a57e785619bd383fe6580b7545899a76d88
Darío Urbina <urbiname@usc.edu>
2020-06-05 13:25:40 -0700

    Body moves forward with constraint DOFs, added a fixed base with a rail attached to it used by the body to move forward

--- pybullet_test_2.py
+++ pybullet_test_2.py
@@ -6,15 +6,17 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
 
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
+cubeStartPos = [0,0,.05]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2_Dario.urdf",cubeStartPos, cubeStartOrientation)
+#With fixed base (Can I do it fixed in a certain plane?)
+boxId = p.loadURDF("r2d2_Dario.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1) 
+#boxId = p.loadURDF("r2d2_Dario.urdf",cubeStartPos, cubeStartOrientation)
 
 for i in range(p.getNumJoints(boxId)):
   print(p.getJointInfo(boxId, i))
 
 for joint in range(p.getNumJoints(boxId)):
-  p.setJointMotorControl2(boxId, joint, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
+  p.setJointMotorControl2(boxId, joint, p.VELOCITY_CONTROL, targetVelocity=10, force=10)
   p.getJointInfo(boxId, joint)
 
 for i in range (10000):
