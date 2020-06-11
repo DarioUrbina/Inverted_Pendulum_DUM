1e8fa216f569a3c7298bae58afd7fe9d49bfd896
Darío Urbina <urbiname@usc.edu>
2020-06-11 12:50:46 -0700

    Test of sinusoidal generation of array to activate motors, shifting sinusoidal signal to activate motors with different patterns

--- Python tests/Declaring arrays.py
+++ Python tests/Declaring arrays.py
@@ -5,14 +5,17 @@ motors=4
 power_blocks=3
 steps=30
 activation_profile = np.zeros(shape=(steps*power_blocks,motors))
-
 line_count=0
 
 
 for i in range (steps):
     for j in range(power_blocks):
         for p in range(motors):
-            activation_profile[line_count][p]=m.sin(i)
+            if (p % 2) == 0:
+                activation_profile[line_count][p]=m.sin(i)
+            else:
+                activation_profile[line_count][p]=m.sin(i+3.14)
+            print (p)
         line_count=line_count+1
 """           
 for i in range (100):
@@ -21,5 +24,6 @@ for i in range (100):
             a[n][k]=m.sin(i)
         n=n+1
 """
-    print(activation_profile)
+
+print(activation_profile)
 
