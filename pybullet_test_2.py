ca640c2fb8d30ab3d8c2fd35658678404d882b51
Darío Urbina <urbiname@usc.edu>
2020-06-11 12:52:24 -0700

    Playing with motors activation patterns considering sin shifting for more distal motors. Modifying xml files, but they are not in this repository

--- pybullet_test_2.py
+++ pybullet_test_2.py
@@ -12,11 +12,22 @@ steps=100
 rows= steps*power_blocks
 activation_profile = np.zeros(shape=(rows,motors))
 line_count=0
-
+"""
 for i in range (steps):
     for j in range(power_blocks):
         for mot in range(motors):
             activation_profile[line_count][mot]=m.sin(i)
+        line_count=line_count+1
+"""
+
+for i in range (steps):
+    for j in range(power_blocks):
+        for mot in range(motors):
+            if (mot % 2) == 0:
+                activation_profile[line_count][mot]=m.sin(i)
+            else:
+                activation_profile[line_count][mot]=m.sin(i+3.14)
+            #print (p)
         line_count=line_count+1
 
 #print (activation_profile)        
@@ -29,9 +40,8 @@ p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,.05]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-#With fixed base (Can I do it fixed in a certain plane?)
 boxId = p.loadURDF("r2d2_Dario.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1) 
-#boxId = p.loadURDF("r2d2_Dario.urdf",cubeStartPos, cubeStartOrientation)
+
 
 for i in range(p.getNumJoints(boxId)):
   print(p.getJointInfo(boxId, i))
@@ -45,7 +55,7 @@ for i in range (rows):
     time.sleep(1./240.)
     for ii in range (1,9):
       #print (ii*2)
-      p.setJointMotorControl2(boxId, ii*2, p.VELOCITY_CONTROL, targetVelocity=10, force=(activation_profile[i][1])*100)
+      p.setJointMotorControl2(boxId, ii*2, p.VELOCITY_CONTROL, targetVelocity=5, force=(activation_profile[i][ii-1])*500)
     
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
 print(cubePos,cubeOrn)
