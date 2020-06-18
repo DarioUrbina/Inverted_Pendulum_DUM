e9a9ab245b0cbd83bda41782fe4eeac53829ea79
Darío Urbina <urbiname@usc.edu>
2020-06-18 15:03:23 -0700

    Generated firts call of a urdf (xml) file. First I wasn't able to call it because two reasons: 1 I wasn't including the right folders, and 2 I wasn't including all the files that Plane urdf file requires

---
+++ Pendulum_exercise/Inverted_pendulum.py
@@ -0,0 +1,28 @@
+import pybullet as p
+import time
+
+p.connect(p.GUI)
+plane = p.loadURDF("plane.urdf")
+
+
+"""
+
+#import data_dario
+#import pybullet_data
+#import time
+
+import math as m
+import numpy as np
+
+
+#p.setAdditionalSearchPath(data_dario.getDataPath())
+#p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+p.setTimeStep(1./500)
+#planeId = p.loadURDF("plane.urdf")
+
+cubeStartPos = [0,0,.05]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+quadruped = p.loadURDF("r2d2_Dario",cubeStartPos, cubeStartOrientation, useFixedBase=1) 
+
+"""
