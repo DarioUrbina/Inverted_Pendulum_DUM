8f46c414054df1475cd9ac4f06a4a2bded52166c
Darío Urbina <urbiname@usc.edu>
2020-06-25 18:34:13 -0700

    pendulum with angle data information shown in real time

--- Pendulum_exercise/Inverted_pendulum.py
+++ Pendulum_exercise/Inverted_pendulum.py
@@ -13,6 +13,13 @@ cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 pendulum = p.loadURDF("Pendulum.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1)
 p.setGravity(0,0,-10)
 
+for i in range(p.getNumJoints(pendulum)):
+    info=p.getJointInfo(pendulum, i)
+    print(p.getJointInfo(pendulum, i))
+    print(info[0])
+    print(p.getJointState(pendulum,info[0]))
+    #print(info([ info[i] for i in [0,1] ]))
+
 for joint in range(p.getNumJoints(pendulum)):
     p.setJointMotorControl2(pendulum, joint, p.VELOCITY_CONTROL, targetVelocity=10, force=0)
     p.getJointInfo(pendulum, joint)
@@ -21,8 +28,12 @@ for joint in range(p.getNumJoints(pendulum)):
 
 for i in range (20000):
     p.stepSimulation()
+    info=p.getJointInfo(pendulum, 3)
+    info2=p.getJointState(pendulum,info[0])
+    print(info2[0])
+    
     time.sleep(1./240.)
-
+    #p.setJointMotorControl2(boxId, 2, p.VELOCITY_CONTROL, targetVelocity=5, force=(activation_profile[i][0])*force_scale)
 p.disconnect()
 
 
