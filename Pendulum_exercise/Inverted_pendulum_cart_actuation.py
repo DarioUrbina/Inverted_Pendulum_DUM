df17341a4ed284843196532ff2ac41df2e8f0df7
Darío Urbina <urbiname@usc.edu>
2020-07-06 12:39:26 -0700

    Adding pulleys and base hight change

--- Pendulum_exercise/Inverted_pendulum_cart_actuation.py
+++ Pendulum_exercise/Inverted_pendulum_cart_actuation.py
@@ -6,10 +6,10 @@ import numpy as np
 p.connect(p.GUI)
 plane = p.loadURDF("plane.urdf")
 
-cubeStartPos = [-2.65,0,.4]
+cubeStartPos = [-2.65,0,1.5]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 #pendulum = p.loadURDF("r2d2_Dario.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1)
-pendulum = p.loadURDF("Pendulum_for_cart_actuation.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1)
+pendulum = p.loadURDF("Pendulum_for_cart_actuation.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
 p.setGravity(0,0,-10)
 
 motorForce=700
