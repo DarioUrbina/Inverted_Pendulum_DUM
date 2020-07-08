fd30e1f41b06a4fb7646de4cdf2ad2b1877daa38
Darío Urbina <urbiname@usc.edu>
2020-07-08 11:33:16 -0700

    Cyclic body chain exploration 1

--- Pendulum_exercise/Inverted_pendulum_tendon_actuation.py
+++ Pendulum_exercise/Inverted_pendulum_tendon_actuation.py
@@ -9,41 +9,35 @@ plane = p.loadURDF("plane.urdf")
 cubeStartPos = [-2.65,0,1.5]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 #pendulum = p.loadURDF("r2d2_Dario.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1)
-pendulum = p.loadURDF("Pendulum_for_tendon_actuation.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
+pendulum = p.loadURDF("Cyclic_body_chain.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
 p.setGravity(0,0,-10)
 
 motorForce=700
 
-proportional_gain = -1500
-integral_gain = -1000
-derivative_gain = 3000
+#proportional_gain = -1500      integral_gain = -1000       derivative_gain = 3000
 
 """to keep cart centered: """
 
-proportional_gain_2 = 800
-integral_gain_2 = 0
-derivative_gain_2 = 0
+#proportional_gain_2 = 800      integral_gain_2 = 0         derivative_gain_2 = 0
 
-previous_pendulum_angle = 0
-previous_cart_position = 0
+#previous_pendulum_angle = 0    previous_cart_position = 0
 
 for i in range(p.getNumJoints(pendulum)):
     """If more links or joints are added, the joint indexes change"""
     info=p.getJointInfo(pendulum, i)
     print(p.getJointInfo(pendulum, i))
-    #print(info[0])
-    #print(p.getJointState(pendulum,info[0]))
-    #print(info([ info[i] for i in [0,1] ]))
+    #print(info[0])    #print(p.getJointState(pendulum,info[0]))    #print(info([ info[i] for i in [0,1] ]))
 
 for joint in range(p.getNumJoints(pendulum)):
     p.setJointMotorControl2(pendulum, joint, p.VELOCITY_CONTROL, targetVelocity=10, force=0)
     p.getJointInfo(pendulum, joint)
 
-p.changeDynamics(pendulum, linkIndex=1 , mass=100, restitution=.1)
+#p.changeDynamics(pendulum, linkIndex=1 , mass=100, restitution=.1)
 
 
 for i in range (20000):
     p.stepSimulation()
+    """
     #info=p.getJointInfo(pendulum, 4)
     pendulum_angle=p.getJointState(pendulum,4)
     cart_position=p.getJointState(pendulum,3)
@@ -54,7 +48,8 @@ for i in range (20000):
     #cart_position_delta_error = 0-cart_position[0]
     print("Angle_Delta_Error:")
     print(angle_delta_error)
-
+    """
+    """
     #PROPPORTIONAL
     p_correction = proportional_gain * pendulum_angle[0]
 
@@ -65,7 +60,7 @@ for i in range (20000):
     #DERIVATIVE
     d_correction = derivative_gain * angle_delta_error
 
-    """to keep cart centered"""
+    """ """to keep cart centered""" """
     #PROPPORTIONAL for cart position
     p_correction_2 = proportional_gain_2 * cart_position[0]
 
@@ -75,14 +70,14 @@ for i in range (20000):
 
     #DERIVATIVE for cart position
     d_correction_2 = derivative_gain_2 * (-cart_position[0])
-    """end of to keep cart centered"""
+    """ """end of to keep cart centered""" """
     
     #Input Signal
     u = p_correction + i_correction + d_correction
     u = u + p_correction_2 + i_correction_2 + d_correction_2
     u=0
     p.setJointMotorControl2(pendulum, 3, p.VELOCITY_CONTROL, targetVelocity=10, force=u)
-
+    """
     
         
     time.sleep(1./240.)
