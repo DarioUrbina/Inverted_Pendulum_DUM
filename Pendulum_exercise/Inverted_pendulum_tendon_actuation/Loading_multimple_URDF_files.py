6b98e8624e1f54bb80994ff8986d8d61a6e73b2f
Darío Urbina <urbiname@usc.edu>
2020-07-13 13:15:21 -0700

    cancelling fixed bases, staring to see some interaction between bodies

--- Pendulum_exercise/Inverted_pendulum_tendon_actuation/Loading_multimple_URDF_files.py
+++ Pendulum_exercise/Inverted_pendulum_tendon_actuation/Loading_multimple_URDF_files.py
@@ -10,15 +10,17 @@ plane = p.loadURDF("plane.urdf")
 cubeStartPos = [-2.15,0,.5]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 
-base_1 = p.loadURDF("Base_1.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
-base_2 = p.loadURDF("Base_2.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
+base_1 = p.loadURDF("Base_1.urdf",cubeStartPos, cubeStartOrientation, flags=p.URDF_USE_SELF_COLLISION)
+base_2 = p.loadURDF("Base_2.urdf",cubeStartPos, cubeStartOrientation, flags=p.URDF_USE_SELF_COLLISION)
 #base_1 = p.loadURDF("Cyclic_body_chain.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
 
 cid = p.createConstraint(base_1, -1, base_1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 1], [0, 0, 1])
-cid2 = p.createConstraint(base_2, -1, base_2, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 2], [0, 0, 0])
+cid2 = p.createConstraint(base_2, -1, base_2, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0])
 #cid2 = p.createConstraint(base_1, -1, base_1, 0, p.JOINT_FIXED, [0, 0, 1], [0, 0, 1], [0, 0, 1]
 
-p.setGravity(0,0,0)
+p.changeConstraint(cid, [1,1,0], [1,0,0], maxForce=50)
+#p.changeConstraint(cid, pivot, jointChildFrameOrientation=orn, maxForce=50)
+p.setGravity(0,0,-10)
 
 
 
@@ -31,6 +33,8 @@ for i in range(p.getNumJoints(base_1)):
 
 for i in range (20000):
     p.stepSimulation()
+    p.changeConstraint(cid, [0,0,0], [1,0,0], maxForce=50)
+
     time.sleep(1./240.)
     
 p.disconnect()
