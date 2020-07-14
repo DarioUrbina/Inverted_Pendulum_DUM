ca51169146cf2990afcd8b4a126d2143699e09a8
Darío Urbina <urbiname@usc.edu>
2020-07-13 19:01:37 -0700

    Firt time a loop is achieved! Still need to fix ho it looks

--- Pendulum_exercise/Inverted_pendulum_tendon_actuation/Loading_multimple_URDF_files.py
+++ Pendulum_exercise/Inverted_pendulum_tendon_actuation/Loading_multimple_URDF_files.py
@@ -27,7 +27,22 @@ jointNameToId = {}
 for i in range(nJoints):
   jointInfo = p.getJointInfo(base_1, i)
   jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]
-  print(jointInfo)
+  #print(jointInfo)
+
+tendon1_4_tendon1_5 = jointNameToId['tendon1_4_tendon1_5']
+
+
+nJoints2 = p.getNumJoints(base_2)
+
+jointNameToId = {}
+
+for i in range(nJoints2):
+  jointInfo2 = p.getJointInfo(base_2, i)
+  jointNameToId[jointInfo2[1].decode('UTF-8')] = jointInfo[0]
+  #print(jointInfo)
+  
+tendon1_4_tendon1_5_2 = jointNameToId['tendon1_4_tendon1_5']
+
 """
 knee_front_rightL_link = jointNameToId['knee_front_rightL_link']
 
@@ -46,9 +61,11 @@ if(1):
 """
 
 
-tendon1_4_tendon1_5 = jointNameToId['tendon1_4_tendon1_5']
-print("LAST TENDON LINK:")
-print(tendon1_4_tendon1_5)
+#tendon1_4_tendon1_5 = jointNameToId['tendon1_4_tendon1_5']
+#tendon1_4_tendon1_5_2 = jointNameToId['tendon1_4_tendon1_5']
+
+#print("LAST TENDON LINK:")
+#print(tendon1_4_tendon1_5)
 #tendon1_4_
 
 #moving_mechanism = p.loadURDF("Cart_tendons_and_pulleys.urdf",cubeStartPos4, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
@@ -59,7 +76,10 @@ print(tendon1_4_tendon1_5)
 #base_1 = p.loadURDF("Cyclic_body_chain.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
 
 cid = p.createConstraint(base_1, -1, base_1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0])
-cid2 = p.createConstraint(base_1, tendon1_4_tendon1_5, cart, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0])
+cid2 = p.createConstraint(base_1, tendon1_4_tendon1_5, cart, -1, p.JOINT_FIXED, [0, 0, 0], [.4, .05, 0], [-.1, .05, 0])
+cid3 = p.createConstraint(base_2, tendon1_4_tendon1_5_2, cart, -1, p.JOINT_FIXED, [0, 0, 0], [.4, .05, 0], [.1, .05, 0])
+
+
 """
 cid = p.createConstraint(quadruped, knee_front_rightR_link, quadruped, knee_front_rightL_link,
                            p.JOINT_POINT2POINT, [0, 0, 0], [0, 0.005, 0.1], [0, 0.01, 0.1])
