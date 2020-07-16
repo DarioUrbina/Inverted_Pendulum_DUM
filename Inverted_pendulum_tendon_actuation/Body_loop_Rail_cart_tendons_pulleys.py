a0131e1ead115a37cd03549c8ca97709f6192949
Darío Urbina <urbiname@usc.edu>
2020-07-16 09:46:19 -0700

    Cleaning code and seraching for erros

--- Inverted_pendulum_tendon_actuation/Body_loop_Rail_cart_tendons_pulleys.py
+++ Inverted_pendulum_tendon_actuation/Body_loop_Rail_cart_tendons_pulleys.py
@@ -12,16 +12,14 @@ cubeStartPos2 = [0,0,1.4]
 cubeStartPos3 = [2.15,0,.75]
 
 PulleyStartOrientation = p.getQuaternionFromEuler([1.570796, 0, 0])  
-cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0]) #**
 cubeStartOrientation2 = p.getQuaternionFromEuler([0,-1.570796,0])
-#cubeStartOrientation2
 
 base_1 = p.loadURDF("Base_1.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
-rail = p.loadURDF("Tendons_Cart_Rail.urdf",cubeStartPos2, cubeStartOrientation2, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
-base_2 = p.loadURDF("Base_1.urdf",cubeStartPos3, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
-"""cart = p.loadURDF("Cart.urdf",flags=p.URDF_USE_SELF_COLLISION)"""
+rail = p.loadURDF("Tendons_Cart_Rail.urdf",cubeStartPos2, cubeStartOrientation2, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION) #**
+base_2 = p.loadURDF("Base_1.urdf",cubeStartPos3, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)           #**
 
-
+""""Getting access and information from specific joints in each body (each body has links and joint described in the URDF files):"""
 nJoints = p.getNumJoints(base_2)
 jointNameToId = {}
 for i in range(nJoints):
@@ -29,56 +27,20 @@ for i in range(nJoints):
   jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]
 Base_pulley1_2 = jointNameToId['Base_pulley1']
 
-nJoints3 = p.getNumJoints(rail)
+nJoints = p.getNumJoints(rail)
 jointNameToId = {}
-for i in range(nJoints3):
-  jointInfo3 = p.getJointInfo(rail, i)
-  jointNameToId[jointInfo3[1].decode('UTF-8')] = jointInfo3[0]
+for i in range(nJoints):
+  jointInfo = p.getJointInfo(rail, i)
+  jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]
 last_tendon_link_1 = jointNameToId['tendon1_13_tendon1_14']
-
 last_tendon_link_2 = jointNameToId['tendon2_13_tendon2_14']
 
-
-
-nJoints2 = p.getNumJoints(base_1)
+nJoints = p.getNumJoints(base_1)
 jointNameToId = {}
-for i in range(nJoints2):
-  jointInfo2 = p.getJointInfo(base_1, i)
-  jointNameToId[jointInfo2[1].decode('UTF-8')] = jointInfo2[0]  
+for i in range(nJoints):
+  jointInfo = p.getJointInfo(base_1, i)
+  jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]  
 Base_pulley1_1 = jointNameToId['Base_pulley1']
-
-"""nJoints4 = p.getNumJoints(rail)
-jointNameToId = {}
-for i in range(nJoints4):
-  jointInfo4 = p.getJointInfo(rail, i)
-  jointNameToId[jointInfo3[1].decode('UTF-8')] = jointInfo4[0]
-last_tendon_link_2 = jointNameToId['tendon2_13_tendon2_14']
-"""
-
-
-
-
-
-""" #********REFERENCE: ***********
-knee_front_rightL_link = jointNameToId['knee_front_rightL_link']
-
-p.setJointMotorControl2(quadruped, knee_front_rightL_link, p.POSITION_CONTROL,
-                            motordir[4] * (kneeangle) * float(aa) / steps)
-
-p.resetJointState(quadruped, knee_front_rightL_link, motordir[4] * kneeangle)
-
-cid = p.createConstraint(quadruped, knee_front_rightR_link, quadruped, knee_front_rightL_link,
-                           p.JOINT_POINT2POINT, [0, 0, 0], [0, 0.005, 0.1], [0, 0.01, 0.1])
-
-if(1):
-    p.setJointMotorControl(quadruped, knee_front_rightL_link, p.VELOCITY_CONTROL, 0,
-                         kneeFrictionForce)
-                         
-"""
-
-
-#tendon1_4_tendon1_5 = jointNameToId['tendon1_4_tendon1_5']
-#tendon1_4_tendon1_5_2 = jointNameToId['tendon1_4_tendon1_5']
 
 #print("LAST TENDON LINK:")
 #print(tendon1_4_tendon1_5)
