b5be03d8298e9676ccf01993a429cb8ac97ee45b
Darío Urbina <urbiname@usc.edu>
2020-07-16 11:28:01 -0700

    Working on making the frames or reference coeherent

--- Inverted_pendulum_tendon_actuation/Body_loop_Rail_cart_tendons_pulleys.py
+++ Inverted_pendulum_tendon_actuation/Body_loop_Rail_cart_tendons_pulleys.py
@@ -3,7 +3,7 @@ import time
 import math as m
 import numpy as np
 import pybullet_data
-#so far revised
+
 p.connect(p.GUI)
 plane = p.loadURDF("plane.urdf")
 
@@ -15,7 +15,7 @@ PulleyStartOrientation = p.getQuaternionFromEuler([1.570796, 0, 0])
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0]) #**
 cubeStartOrientation2 = p.getQuaternionFromEuler([0,-1.570796,0])
 
-base_1 = p.loadURDF("Base_1.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
+base_1 = p.loadURDF("Base_2.urdf",cubeStartPos, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
 rail = p.loadURDF("Tendons_Cart_Rail.urdf",cubeStartPos2, cubeStartOrientation2, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION) #**
 base_2 = p.loadURDF("Base_1.urdf",cubeStartPos3, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)           #**
 
@@ -33,31 +33,22 @@ for i in range(nJoints):
   jointInfo = p.getJointInfo(rail, i)
   jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]
 last_tendon_link_1 = jointNameToId['tendon1_13_tendon1_14']
-last_tendon_link_2 = jointNameToId['tendon2_13_tendon2_14']
-
+#last_tendon_link_2 = jointNameToId['tendon2_13_tendon2_14']
+"""
 nJoints = p.getNumJoints(base_1)
 jointNameToId = {}
 for i in range(nJoints):
   jointInfo = p.getJointInfo(base_1, i)
   jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]  
 Base_pulley1_1 = jointNameToId['Base_pulley1']
-
+"""
 #print("LAST TENDON LINK:")
 #print(tendon1_4_tendon1_5)
 
-
-#moving_mechanism = p.loadURDF("Cart_tendons_and_pulleys.urdf",cubeStartPos4, cubeStartOrientation, useFixedBase=1, flags=p.URDF_USE_SELF_COLLISION)
-
-
-#cid = p.createConstraint(base_1, -1, base_1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0])
-
-#WORKING WELL:
 cid2 = p.createConstraint(base_2, Base_pulley1_2, rail, last_tendon_link_1, p.JOINT_FIXED, [0, 0, 1], [0, 0, 0], [-.56, 0, 0])
 
-cid4 = p.createConstraint(base_1, Base_pulley1_1, rail, last_tendon_link_2, p.JOINT_FIXED, [0, 0, 1], [0, 0, 0], [-.56, 0, 0])
-"""
-cid3 = p.createConstraint(base_2, tendon1_4_tendon1_5_2, cart, -1, p.JOINT_FIXED, [0, 0, 0], [.45, .05, 0], [.1, .05, 0])
-"""
+#cid4 = p.createConstraint(base_1, Base_pulley1_1, rail, last_tendon_link_2, p.JOINT_FIXED, [0, 0, 1], [0, 0, 0], [-.56, 0, 0])
+
 
 """ #********REFERENCE: ***********
 cid = p.createConstraint(quadruped, knee_front_rightR_link, quadruped, knee_front_rightL_link,
